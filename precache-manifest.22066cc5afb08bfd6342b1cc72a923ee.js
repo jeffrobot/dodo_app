2ff1933e005cd3e4d74a6c918ae6adcf2e9c7038
JangHan Yoon <jeffrobot99@gmail.com>
2020-03-07 16:03:28 +0900

    Updates

---
+++ precache-manifest.22066cc5afb08bfd6342b1cc72a923ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5843daa13c694f9a5ecb51d77cb6993",
+    "url": "/dodo_app/index.html"
+  },
+  {
+    "revision": "e57299aecf2433514ba8",
+    "url": "/dodo_app/static/css/main.b7cf6848.chunk.css"
+  },
+  {
+    "revision": "f8db01243e6671f00218",
+    "url": "/dodo_app/static/js/2.63f69968.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/dodo_app/static/js/2.63f69968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e57299aecf2433514ba8",
+    "url": "/dodo_app/static/js/main.60a667cf.chunk.js"
+  },
+  {
+    "revision": "462f61aedfa572e62415",
+    "url": "/dodo_app/static/js/runtime-main.4d41da71.js"
+  }
+]);
